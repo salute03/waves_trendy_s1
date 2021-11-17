e7586290d8fd961ab6dc0824243b757e37e77bba
salute03 <oluwasegunogunsakin@gmail.com>
2021-11-17 16:24:43 +0000

    Deployment in progress, added allowed host

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn waves_trendy.wsgi:application
--- waves_trendy/settings.py
+++ waves_trendy/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=t(+2i@$8qla9c18okpdbij=c0(--pjty4x1_2&djv5xci*6=t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['salute03-waves-trendy.heroku.com', 'localhost']
 
 
 # Application definition
@@ -122,12 +122,18 @@ WSGI_APPLICATION = 'waves_trendy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 
