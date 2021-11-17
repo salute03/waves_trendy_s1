fcc05ad17b862c4ac78c6c3af8a95d027d56039e
salute03 <oluwasegunogunsakin@gmail.com>
2021-11-17 17:34:29 +0000

    it
    Removed Secret key and set debug

--- waves_trendy/settings.py
+++ waves_trendy/settings.py
@@ -21,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=t(+2i@$8qla9c18okpdbij=c0(--pjty4x1_2&djv5xci*6=t'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
+
 
 ALLOWED_HOSTS = ['salute03-waves-trendy.herokuapp.com', 'localhost']
 
